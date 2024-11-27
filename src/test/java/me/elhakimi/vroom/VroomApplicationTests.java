6b7ad585d21021e19ec00292f0f83a4233688c63
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2024-11-27 17:11:37 +0100

    first commit

---
+++ src/test/java/me/elhakimi/vroom/VroomApplicationTests.java
@@ -0,0 +1,13 @@
+package me.elhakimi.vroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
