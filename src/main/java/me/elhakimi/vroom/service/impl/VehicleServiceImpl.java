57d9145d22f1c337aa0071b40e580bd009770e8b
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2025-01-14 18:01:41 +0100

    vehicle find All with pagination

--- src/main/java/me/elhakimi/vroom/service/impl/VehicleServiceImpl.java
+++ src/main/java/me/elhakimi/vroom/service/impl/VehicleServiceImpl.java
@@ -7,10 +7,10 @@ import me.elhakimi.vroom.domain.Vehicle;
 import me.elhakimi.vroom.domain.enums.VehicleStatus;
 import me.elhakimi.vroom.repository.VehicleRepository;
 import me.elhakimi.vroom.service.UserService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
 
 @Service
@@ -31,9 +31,49 @@ public class VehicleServiceImpl {
 
     }
 
+    public Vehicle update(Vehicle vehicle){
+
+        AppUser appUser = (AppUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if(vehicle.getUser() != appUser) throw new RuntimeException("You are not allowed to update this vehicle");
+
+        vehicle.setUpdatedAt(LocalDateTime.now());
+        vehicle.setStatus(VehicleStatus.PENDING);
+
+        return vehicleRepository.save(vehicle);
+
+    }
 
 
+    public void archive(Long id){
+        Vehicle vehicle = vehicleRepository.findById(id).orElseThrow(() -> new RuntimeException("Vehicle not found"));
+        vehicle.setArchived(true);
+        vehicleRepository.save(vehicle);
+    }
 
+    public void delete(Long id){
+        Vehicle vehicle = vehicleRepository.findById(id).orElseThrow(() -> new RuntimeException("Vehicle not found"));
+        vehicleRepository.delete(vehicle);
+    }
 
+    public Vehicle approve(Long id){
+        Vehicle vehicle = vehicleRepository.findById(id).orElseThrow(() -> new RuntimeException("Vehicle not found"));
+        vehicle.setStatus(VehicleStatus.PUBLISHED);
+        return vehicleRepository.save(vehicle);
+    }
+
+    public Vehicle reject(Long id){
+        Vehicle vehicle = vehicleRepository.findById(id).orElseThrow(() -> new RuntimeException("Vehicle not found"));
+        vehicle.setStatus(VehicleStatus.REJECTED);
+        return vehicleRepository.save(vehicle);
+    }
+
+    public Vehicle findById(Long id){
+        return vehicleRepository.findById(id).orElseThrow(() -> new RuntimeException("Vehicle not found"));
+    }
+
+    public Page<Vehicle> findAll(Pageable pageable) {
+        return vehicleRepository.findAll(pageable);
+    }
 
 }
--- src/main/java/me/elhakimi/vroom/web/admin/VehicleController.java
+++ src/main/java/me/elhakimi/vroom/web/admin/VehicleController.java
@@ -6,12 +6,13 @@ import me.elhakimi.vroom.domain.Vehicle;
 import me.elhakimi.vroom.dto.user.response.UserDetails;
 import me.elhakimi.vroom.dto.user.response.VehicleResponse;
 import me.elhakimi.vroom.service.impl.VehicleServiceImpl;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.print.Pageable;
 
 @RestController
 @RequestMapping("/admin/vehicles")
@@ -34,4 +35,57 @@ public class VehicleController {
             }
         }
 
+        @PostMapping("/update")
+        public ResponseEntity<Object> updateVehicle(@RequestBody Vehicle vehicle) {
+
+            Vehicle updateVehicle = vehicleServiceImpl.update(vehicle);
+            if(updateVehicle != null) {
+                return ResponseEntity.ok(VehicleResponse.from(updateVehicle , UserDetails.from(updateVehicle.getUser())));
+            }
+            else {
+                return ResponseEntity.badRequest().body(vehicle);
+            }
+        }
+
+        @PostMapping("/archive")
+
+        public ResponseEntity<Object> archiveVehicle(@RequestBody Long id) {
+            vehicleServiceImpl.archive(id);
+            return ResponseEntity.ok("Vehicle archived successfully");
+        }
+
+        @PostMapping("/delete")
+
+        public ResponseEntity<Object> deleteVehicle(@RequestBody Long id) {
+            vehicleServiceImpl.delete(id);
+            return ResponseEntity.ok("Vehicle deleted successfully");
+        }
+
+        @PostMapping("/approve")
+
+        public ResponseEntity<Object> approveVehicle(@RequestBody Long id) {
+            Vehicle vehicle = vehicleServiceImpl.approve(id);
+            return ResponseEntity.ok(VehicleResponse.from(vehicle , UserDetails.from(vehicle.getUser())));
+        }
+
+        @PostMapping("/reject")
+
+        public ResponseEntity<Object> rejectVehicle(@RequestBody Long id) {
+            Vehicle vehicle = vehicleServiceImpl.reject(id);
+            return ResponseEntity.ok(VehicleResponse.from(vehicle , UserDetails.from(vehicle.getUser())));
+        }
+
+        @PostMapping("/find")
+        public ResponseEntity<Object> findVehicle(@RequestBody Long id) {
+            Vehicle vehicle = vehicleServiceImpl.findById(id);
+            return ResponseEntity.ok(VehicleResponse.from(vehicle , UserDetails.from(vehicle.getUser())));
+        }
+
+    @GetMapping("/all")
+    public ResponseEntity<Object> findAllVehicles(@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "10") int size) {
+        PageRequest pageable = PageRequest.of(page-1, size);
+        return ResponseEntity.ok(vehicleServiceImpl.findAll(pageable).map(
+                vehicle ->
+                        VehicleResponse.from(vehicle , UserDetails.from(vehicle.getUser()))));
+    }
 }
