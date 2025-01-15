65dd33de325d1ced3220f06daeec0dd86f1818d3
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2025-01-15 18:54:47 +0100

    add upload images service for upload to aws S3

--- src/main/java/me/elhakimi/vroom/security/PasswordEncoder.java
+++ src/main/java/me/elhakimi/vroom/security/PasswordEncoder.java
@@ -1,5 +1,11 @@
 package me.elhakimi.vroom.security;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,8 +18,4 @@ public class PasswordEncoder {
     public BCryptPasswordEncoder bcryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-
-
-
 }
